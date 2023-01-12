af146e54e946c5253a06777bcacb80b1e029c20e
Dmitrii Sobin <neo7976@yandex.ru>
2023-01-12 14:02:08 +0300

    Успешная запись в базу данных id и типа работы таймера

--- src/main/java/com/example/javapomodorotelegrambotspring/TimerRepository.java
+++ src/main/java/com/example/javapomodorotelegrambotspring/TimerRepository.java
@@ -8,8 +8,7 @@ import java.sql.SQLException;
 public class TimerRepository {
 
     private final DataSource dataSource;
-//    private static final String INSERT_QUERY = "insert into user_timer(user_id, timer_type) values (?, ?);";
-    private static final String INSERT_QUERY = "insert into sobinda.user_timer(user_id, timer_type) values ('2', 'test');";
+    private static final String INSERT_QUERY = "insert into sobinda.user_timer(user_id, timer_type) values (?, ?);";
 
     public TimerRepository(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -18,8 +17,8 @@ public class TimerRepository {
     public void save(String userId, String timerType) {
         try (Connection connection = dataSource.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY);
-//            preparedStatement.setString(1, userId);
-//            preparedStatement.setString(2, timerType);
+            preparedStatement.setString(1, userId);
+            preparedStatement.setString(2, timerType);
             preparedStatement.execute();
         } catch (SQLException e) {
             throw new RuntimeException(e);
